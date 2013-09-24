96a13f57c594f62262faabecab7b28b5222cbf52
Jun Chen <junc76@gmail.com>
2013-09-24 14:18:09 +0800

    Add the NUnit-2.6.2 and the test example

---
+++ MyFirstTest/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MyFirstTest
+{
+    public class MyTest
+    {
+        [Test]
+        public void Test1()
+        {
+            Console.WriteLine("Test1 Pass");
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Console.WriteLine("Test2 Fail");
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Console.WriteLine("Test3 Ignore");
+            Assert.Ignore();
+        }
+    }
+}
