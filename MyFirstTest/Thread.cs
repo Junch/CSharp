2d2fb88885e913db4570f67c9d46e843f6e1cb50
Jun Chen <junc76@gmail.com>
2013-09-25 09:56:43 +0800

    CSharp: Add tests related to thread

---
+++ MyFirstTest/Thread.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+using NUnit.Framework;
+
+namespace MyFirstTest
+{
+    class ThreadStudy
+    {
+        [Test, Ignore]
+        public void task()
+        {
+            const int repetitions = 1000;
+            Task task = new Task(() =>
+            {
+                for (int count = 0; count < repetitions; count++)
+                {
+                    Console.Write('-');
+                }
+            });
+            task.Start();
+            for (int count = 0; count < repetitions; count++)
+            {
+                Console.Write('.');
+            }
+            // Wait until the Task completes
+            task.Wait();
+        }
+
+        [Test]
+        public void sleep1()
+        {
+            Thread.Sleep(2000);
+            Console.Beep();
+        }
+
+        [Test]
+        public void sleep2()
+        {
+            ManualResetEvent dummy = new ManualResetEvent(false);
+            dummy.WaitOne(2000);
+            Console.Beep();
+        }
+    }
+}
