7cb1878ff16bb259b83732d2d25a41f47180203a
Jun Chen <junc76@gmail.com>
2014-10-29 11:10:15 +0800

    Pass all of the unit tests

--- MoqSample/Program.cs
+++ MoqSample/Program.cs
@@ -39,7 +39,15 @@ namespace MoqSample
 
     public class MinimumDiscountHelper : IDiscountHelper {
         public decimal ApplyDiscount(decimal totalParam) {
-            throw new NotImplementedException();
+            if (totalParam < 0) {
+                throw new ArgumentOutOfRangeException();
+            } else if (totalParam > 100) {
+                return totalParam * 0.9M;
+            } else if (totalParam >= 10 && totalParam <= 100) {
+                return totalParam - 5;
+            } else {
+                return totalParam;
+            }
         }
     }
 
