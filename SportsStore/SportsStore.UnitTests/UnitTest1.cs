361240ab88bbc871fc3d2b5607c3d78dfc7d5edf
Jun Chen <junc76@gmail.com>
2014-11-05 10:42:38 +0800

    Display page links

--- SportsStore/SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore/SportsStore.UnitTests/UnitTest1.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
 using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web.Mvc;
+using SportsStore.WebUI.HtmlHelpers;
 
 namespace SportsStore.UnitTests
 {
@@ -14,6 +18,7 @@ namespace SportsStore.UnitTests
         [TestMethod]
         public void Can_Paginate()
         {
+            // Arrange
             Mock<IProductRepository> mock = new Mock<IProductRepository>();
             mock.Setup(m=>m.Products).Returns( new Product[]{
                 new Product {ProductID = 1, Name = "P1"},
@@ -23,15 +28,43 @@ namespace SportsStore.UnitTests
                 new Product {ProductID = 5, Name = "P5"},
             });
 
+            // Act
             ProductController controller = new ProductController(mock.Object);
             controller.PageSize = 3;
 
-            IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2).Model;
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
             
-            Product[] prodArray = result.ToArray();
+            // Assert
+            Product[] prodArray = result.Products.ToArray();
             Assert.IsTrue(prodArray.Length == 2);
             Assert.AreEqual(prodArray[0].Name, "P4");
             Assert.AreEqual(prodArray[1].Name, "P5");
+        }
+
+        [TestMethod]
+        public void Can_Generate_Page_Links()
+        {
+            // Arrange - define an HTML helper - we need to do this
+            // in order to apply the extension method
+            HtmlHelper myHelper = null;
+            // Arrange - create PagingInfo data
+            PagingInfo pagingInfo = new PagingInfo
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+            // Arrange - set up the delegate using a lambda expression
+            Func<int, string> pageUrlDelegate = i => "Page" + i;
+
+            // Act
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
+
+            // Assert
+            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Page1"">1</a>"
+            + @"<a class=""btn btn-default btn-primary selected"" href=""Page2"">2</a>"
+            + @"<a class=""btn btn-default"" href=""Page3"">3</a>",
+            result.ToString());
         }
     }
 }
