5edde8c4ae590570d539b74ef29f5a4e25334dac
Jun Chen <junc76@gmail.com>
2014-11-06 16:44:58 +0800

    Dinner: Add a test project

---
+++ Dinner/Dinner/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Dinner.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dinner.Controllers
+{
+    public class UserController : Controller
+    {
+        private IUserRepository repository;
+
+        public UserController(IUserRepository repos) {
+            this.repository = repos;
+        }
+
+        public ViewResult List() {
+            return View(repository.Users);
+        }
+    }
+}
---
+++ Dinner/Dinner/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Ninject;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Moq;
+using Dinner.Models;
+
+namespace Dinner.Infrastructure {
+    public class NinjectDependencyResolver : IDependencyResolver {
+        private IKernel kernel;
+        public NinjectDependencyResolver(IKernel kernelParam) {
+            kernel = kernelParam;
+            AddBindings();
+        }
+        public object GetService(Type serviceType) {
+            return kernel.TryGet(serviceType);
+        }
+        public IEnumerable<object> GetServices(Type serviceType) {
+            return kernel.GetAll(serviceType);
+        }
+        private void AddBindings() {
+            Mock<IUserRepository> mock = new Mock<IUserRepository>();
+            mock.Setup(m => m.Users).Returns(new List<User> {
+                new User { Name = "Tom Hanks", Email = "tom@goole.com" },
+                new User { Name = "John Smith", Email = "John.Smith@google.com"},
+                new User { Name = "Daniel Chen", Email = "Jun.Chen@autodesk.com"}
+            });
+
+            kernel.Bind<IUserRepository>().ToConstant(mock.Object);
+        }
+    }
+}
